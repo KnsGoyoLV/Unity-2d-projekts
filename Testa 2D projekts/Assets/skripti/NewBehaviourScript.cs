10fdad56e22ba1ce4e7ad82163d2f9a8709fbf0b
Hlz HD <daniss404@gmx.com>
2021-12-03 13:36:38 +0200

    work in progress

---
+++ Testa 2D projekts/Assets/skripti/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string teksts;
+
+    public GameObject text_input;
+
+    public GameObject Show_text;
+
+    public void text_Save()
+    {
+        teksts = InputField.GetComponoment<Text>().Text;
+
+
+    }
+
+
+
+}
